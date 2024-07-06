93cdaad24a413920da6417ae12ee6a84ebee8c47
Ruzlex <alexruzanov2004@gmail.com>
2024-07-06 22:01:30 +0500

    Поправил конвертацию из одного диапазона в другой

--- Assets/Scripts/Misc/Helpers.cs
+++ Assets/Scripts/Misc/Helpers.cs
@@ -53,4 +53,13 @@ public static class Helpers
         });
 #endif
     }
+    
+    public static float ConvertRange(
+        float originalStart, float originalEnd, // original range
+        float newStart, float newEnd, // desired range
+        float value) // value to convert
+    {
+        double scale = (double)(newEnd - newStart) / (originalEnd - originalStart);
+        return (float)(newStart + ((value - originalStart) * scale));
+    }
 }
