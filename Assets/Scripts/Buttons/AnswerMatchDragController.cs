6563f0ae50cfaab8de86eaaa395d626ebb5a7b26
Ruzlex <alexruzanov2004@gmail.com>
2024-07-22 00:55:14 +0500

    Match вопрос в редакторе, начало работы над поддержкой типа вопроса в игре

---
+++ Assets/Scripts/Buttons/AnswerMatchDragController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnswerMatchDragController : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    public int answerIndex;
+    
+    private GameObject mainContent;
+    private Vector3 currentPosition;
+    private RectTransform rectTransform;
+    private int totalChild;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        rectTransform = GetComponent<RectTransform>();
+        currentPosition = transform.localPosition;
+        mainContent = transform.parent.gameObject;
+        totalChild = mainContent.transform.childCount;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform,
+                eventData.position, Camera.main, out var localPoint))
+        { // TODO: поправить всё это дело
+            rectTransform.anchoredPosition =
+                new Vector3(rectTransform.anchoredPosition.x, localPoint.y, 0);
+        }
+        for (int i = 0; i < totalChild; i++)
+        {
+            if (i != transform.GetSiblingIndex())
+            {
+                Transform otherTransform = mainContent.transform.GetChild(i);
+                int distance = (int) Vector3.Distance(transform.localPosition,
+                    otherTransform.localPosition);
+                if (distance <= 10)
+                {
+                    Vector3 otherTransformOldPosition = otherTransform.localPosition;
+                    otherTransform.localPosition = new Vector3(otherTransform.localPosition.x, currentPosition.y,
+                        0);
+                    transform.localPosition = new Vector3(transform.localPosition.x, otherTransformOldPosition.y,
+                       0);
+                    transform.SetSiblingIndex(otherTransform.GetSiblingIndex());
+                    currentPosition = transform.localPosition;
+                }
+            }
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.localPosition = currentPosition;
+    }
+}
